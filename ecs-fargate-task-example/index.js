588c95673b9e3cadf40f9f1db6b069be801c7708
Brian Pfeil <brian.pfeil@gmail.com>
2022-07-29 17:26:30 +0000

    added access public internet

--- ecs-fargate-task-example/index.js
+++ ecs-fargate-task-example/index.js
@@ -1,1 +1,12 @@
-console.log('hello from ecs-fargate-task-example')
+const axios = require('axios');
+
+(async () => {
+
+    console.log('hello from ecs-fargate-task-example\n\n')
+
+    // access public internet
+    const resp = await axios.get('https://postman-echo.com/get')
+    console.log(`GET https://postman-echo.com/get \n\n ${JSON.stringify(resp.data, null,2)}`)
+  
+})()
+
