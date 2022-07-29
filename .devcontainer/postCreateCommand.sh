4ddd0769915db6c89666fe30c69b865ff7ed110c
Brian Pfeil <brian.pfeil@gmail.com>
2022-07-28 20:29:50 -0400

    added devcontainer

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -ex
+
+brew install \
+    jq \
+    yq \
+    pipx \
+    pyenv \
+    tree \
+    terraform
+
+# pyenv setup
+pyenv install 3.9.13
+pyenv global 3.9.13
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+
+source ~/.bashrc
