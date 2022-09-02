91ff4aa0b03cb710ec14f4b1723929d5e9c722bb
Brian Pfeil <brian.pfeil@gmail.com>
2022-09-02 11:22:38 -0400

    devcontainer adds

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -8,7 +8,9 @@ brew install \
     pipx \
     pyenv \
     tree \
-    terraform
+    terraform \
+    tmux \
+    cfn-lint
 
 # pyenv setup
 pyenv install 3.9.13
@@ -17,5 +19,9 @@ pyenv global 3.9.13
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+
+# install IJavascript is a javascript kernel for the Jupyter notebook
+npm install -g ijavascript
+ijsinstall
 
 source ~/.bashrc
